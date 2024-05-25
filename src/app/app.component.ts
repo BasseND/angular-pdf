dd229f80baacb469d297032374af66a2a5636d2b
BasseND <ndiayebasse@gmail.com>
2024-05-25 23:48:39 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { jsPDF } from 'jspdf';
+import  pdfMake  from 'pdfmake/build/pdfmake';
+import  pdfFonts  from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
   selector: 'app-root',
